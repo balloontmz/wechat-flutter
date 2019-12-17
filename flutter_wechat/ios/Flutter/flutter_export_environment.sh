c6e547310072ed893e2d95679b0dfebdc8833282
tomtiddler <balloontmz@gmail.com>
2019-12-17 17:19:08 +0800

    初始化

---
+++ flutter_wechat/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/tomtiddler/flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/New/Code/flutter-code/wechat-flutter/flutter_wechat"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/tomtiddler/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
